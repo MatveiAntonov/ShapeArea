f8a87d2afdc76464c8db7bbd74985432331113d3
ProsTuha <tuhalf2002@mail.ru>
2023-03-15 11:58:30 +0300

    Circle class inherits from Shape abstraction

---
+++ ShapeArea/Shapes/Circle.cs
@@ -0,0 +1,40 @@
+﻿namespace ShapeArea.Shapes;
+
+public class Circle : Shape
+{
+    #region Private fields
+
+    /// <summary>
+    /// Радиус окружности
+    /// </summary>
+    private readonly double _radius;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Окружность
+    /// </summary>
+    /// <param name="radius">Радиус окружности</param>
+    /// <exception cref="ArgumentOutOfRangeException">При отрицательном радиусе</exception>
+    public Circle(double radius)
+    {
+        if (radius < 0)
+        {
+            throw new ArgumentOutOfRangeException("Радиус должен быть положительным числом");
+        }
+        _radius = radius;
+    }
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Вычислить площадь окружности
+    /// </summary>
+    /// <returns>Площадь окружности</returns>
+    public override double CalculateArea() => Math.PI * Math.Pow(_radius, 2);
+
+    #endregion
+}
